ad7e5ff8ba73d20cce30423b3692bb6bd0b58e66
Kevin Tan <kevintannnn05@gmail.com>
2024-07-10 20:09:01 +0800

    added resume page

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.ScrollY > 130);
 })
+
+function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.style.display === 'none' || section.style.display === '') {
+        section.style.display = 'block';
+    } else {
+        section.style.display = 'none';
+    }
+}
