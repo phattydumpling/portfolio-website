a7323b8e8972761f9d46ae12a51359b9a82cdc2d
Kevin Tan <kevintannnn05@gmail.com>
2024-07-13 16:17:37 +0800

    added html for contact page. added media queries for smaller window sizes. added js for menu and also typed words for homepage.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,26 @@ function toggleSection(sectionId) {
     if (section.style.display === 'none' || section.style.display === '') {
         section.style.display = 'block';
     } else {
-        section.style.display = 'none';
+        section.style .display = 'none';
     }
 }
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
+var typed = new Typed(".input", {
+    strings: ["Computer Science Student.", "Cybersecurity Student."],
+    typeSpeed: 80,
+    backSpeed: 30,
+    loop: true
+});
